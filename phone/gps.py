c1bf928d9e75e6a22bae0f989d35964131002378
finkegabriel <finkegabriel@gmail.com>
2025-03-31 21:31:01 -0700

    update with phone and jetson

--- phone/gps.py
+++ phone/gps.py
@@ -14,7 +14,7 @@ async def send_gps(websocket, path):
             }
             await websocket.send(json.dumps(gps_data))
 
-start_server = websockets.serve(send_gps, "0.0.0.0", 8765)
+websockets.serve(send_gps, "0.0.0.0", 8765)
 
-asyncio.get_event_loop().run_until_complete(start_server)
+#asyncio.get_event_loop().run_until_complete(start_server)
 #asyncio.get_event_loop().run_forever()
