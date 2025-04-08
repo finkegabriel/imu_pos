6d77e96724439eb167476371655f0943f43cdc4d
jetson <finkegabriel@gmail.com>
2025-04-08 11:55:35 -0700

    update

--- main/sqlite_logger.py
+++ main/sqlite_logger.py
@@ -11,40 +11,63 @@ host_name = "ws://192.168.8.183:8098"
 
 # Create a wrapper function to handle the path parameter
 def create_message_handler(ws,message):
-    print(ws.url)        
-    # sensor_val = []
-    # print("Connecting to sensor...")
-    # values = json.loads(message)['values']
-    # x = values[0]
-    # y = values[1]
-    # z = values[2]
-    # print("msg ", sensor_val)
-    # print("x = ", x, "y = ", y, "z = ", z)
+    sensor = ws.url.split(".")[len(ws.url.split("."))-1]
+    if(sensor == "gyroscope"):
+        sensor_val = []
+        print("Connecting to sensor...")
+        values = json.loads(message)['values']
+        x = values[0]
+        y = values[1]
+        z = values[2]
+        print("msg ", sensor_val)
+        print("x = ", x, "y = ", y, "z = ", z)
+            
+        # Insert IMU sample
+        cur.execute("""
+                INSERT INTO imu_data (timestamp, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z)
+                VALUES (?, ?, ?, ?, ?, ?, ?)""",
+                (time.time(), 0.0, 0.0, 0.0, x, y, z))
         
-    # #Insert IMU sample
+        # Insert GPS sample
+        cur.execute("""
+            INSERT INTO gps_data (timestamp, latitude, longitude, altitude, speed, fix_quality, num_satellites, hdop)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+            (time.time(), 37.4219983, -122.084, 15.0, 1.4, 1, 7, 0.9))
 
-    # cur.execute("""
-    #         INSERT INTO imu_data (timestamp, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z)
-    #         VALUES (?, ?, ?, ?, ?, ?, ?)""",
-    #         (time.time(), 0.1, 0.0, 9.8, x, y, z))
-
-            # Insert GPS sample
-            # cur.execute("""
-            # INSERT INTO gps_data (timestamp, latitude, longitude, altitude, speed, fix_quality, num_satellites, hdop)
-            # VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
-            # (time.time(), 37.4219983, -122.084, 15.0, 1.4, 1, 7, 0.9))
+    if(sensor == "accelerometer"):
+        sensor_val = []
+        print("Connecting to sensor...")
+        values = json.loads(message)['values']
+        x = values[0]
+        y = values[1]
+        z = values[2]
+        print("msg ", sensor_val)
+        print("x = ", x, "y = ", y, "z = ", z)
+            
+        # Insert IMU sample
+        cur.execute("""
+                INSERT INTO imu_data (timestamp, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z)
+                VALUES (?, ?, ?, ?, ?, ?, ?)""",
+                (time.time(), x, y, z,0.0, 0.0, 0.0))
+        
+        # Insert GPS sample
+        cur.execute("""
+            INSERT INTO gps_data (timestamp, latitude, longitude, altitude, speed, fix_quality, num_satellites, hdop)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+            (time.time(), 37.4219983, -122.084, 15.0, 1.4, 1, 7, 0.9))
+    
 
 def on_error(ws, error):
-    print("error occurred ", error)
+    print("error occurred ")
     
 def on_close(ws, close_code, reason):
-    print("connection closed : ", reason)
+    print("connection closed : ")
 
 def on_message(ws, message):
-    print(f"[{ws.url}] Received: {message}")
-
+    print(f"[{ws.url}] Received:")
+    create_message_handler(ws,message)
 def on_open(ws):
-    print(f"[{ws.url}] Connection opened")
+    print(f"Connection opened")
 
 def create_ws(url):
     ws = websocket.WebSocketApp(
